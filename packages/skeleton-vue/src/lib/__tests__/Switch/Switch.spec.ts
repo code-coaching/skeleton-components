ac31ae677db2c59f9dd96d21d061b03c251b7ad6
Bart Duisters <bartduisters@bartduisters.com>
2024-07-15 14:42:52 +0200

    fix: switch test

--- packages/skeleton-vue/src/lib/__tests__/Switch/Switch.spec.ts
+++ packages/skeleton-vue/src/lib/__tests__/Switch/Switch.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { Switch } from '@/lib';
+import { Switch } from '../..';
 import { mount } from '@vue/test-utils';
 
 describe('Switch', () => {
