3ee530da11d092552df769601670e42f4adc59e1
Bart Duisters <bartduisters@bartduisters.com>
2024-07-16 14:32:05 +0200

    feat: switch page

--- packages/skeleton-angular/src/app/pages/switch/switch.component.ts
+++ packages/skeleton-angular/src/app/pages/switch/switch.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Switch } from 'lib';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-switch',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, Switch],
   templateUrl: './switch.component.html',
   styleUrl: './switch.component.css',
 })
-export class SwitchComponent {}
+export class SwitchComponent {
+  disturb = false;
+  notifications = true;
+  disabled = false;
+  icons = true;
+  lightswitch = false;
+  compact = false;
+  JSON = JSON;
+}
