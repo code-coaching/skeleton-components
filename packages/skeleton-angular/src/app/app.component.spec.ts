2f71e778ba59a8206b32bd1cb7019df3df8cad66
Bart Duisters <bartduisters@bartduisters.com>
2024-07-19 12:20:47 +0200

    chore: formatting - remove old test

--- packages/skeleton-angular/src/app/app.component.spec.ts
+++ packages/skeleton-angular/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'skeleton-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('skeleton-angular');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
